3103bdad18e35466b4f3a87d7ab7d1a1b74af960
EC2 Default User <ec2-user@ip-172-31-27-49.us-west-2.compute.internal>
2016-07-09 09:38:31 +0000

    change config

--- src/config.php
+++ src/config.php
@@ -1,13 +1,13 @@
 <?php
 //DB config
-define('DBHost', '');
-define('DBUser', '');
-define('DBPass', '');
+define('DBHost', 'demo.ca9zvlaustjf.us-west-2.rds.amazonaws.com');
+define('DBUser', 'demo');
+define('DBPass', 'mysqldemo');
 
 //AWS Key config
 //define('AWSKey', '');
 //define('AWSSecret', '');
-define('AWSRegion', '');
+define('AWSRegion', 'us-west-2');
 
 //AWS S3 config
-define('S3Bucket', '');
+define('S3Bucket', 'nitatas3');
